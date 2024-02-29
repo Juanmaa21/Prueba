a4b3345637728693429a6d39fa7fbbdd79dd3fc4
Juanma21 <125654554+Juanmaa21@users.noreply.github.com>
2024-02-29 11:24:20 +0000

    Ejercicio Mineria Datos

--- Prueba.java
+++ Prueba.java
@@ -1,7 +1,60 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 public class Prueba{
 
     public static void main(String[] args){
+        ArrayList<Integer> listaNum = generarLista();
+        System.out.println(listaNum);
+        System.out.println(mineriaDatos(listaNum));
+    }
 
-        System.out.println("hola");
+    public static ArrayList<Integer> generarLista(){
+        ArrayList<Integer> lista = new ArrayList<>();
+        int longitud = (int) (Math.random()*20+1);
+
+        for (int i = 0; i < longitud; i++) {
+            lista.add((int) (Math.random()*200+1));
+        }
+
+        return lista;
+
+    }
+
+    public static int sumarCifras(int n){
+        int suma = 0;
+        int cifra = 0;
+
+        while (n != 0) {         
+            cifra = n % 10;       
+            suma = suma + cifra;   
+            n = n / 10;            
+        }
+
+        return suma;
+    }
+
+    public static int sumarNumeros(ArrayList<Integer> numeros){
+        int sol = 0;
+        for (Integer numero : numeros) { 
+            sol+= numero.intValue();
+        }
+        return sol;
+    }
+
+    public static int mineriaDatos(ArrayList<Integer> numeros){
+        int suma = sumarNumeros(numeros);
+        int sumaCifras = sumarCifras(suma);
+
+        if(sumaCifras == 0){
+            return 0;
+        }
+
+        if(sumaCifras % 9 == 0){
+            return 9;
+        }else{
+            return sumaCifras % 9;
+        }
     }
 }
